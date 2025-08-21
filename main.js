91e7dbe9f6943935d6b6b0748044b499ff8d7f57
Sanjh86 <sanjhsolanki@google.com>
2025-08-21 21:53:37 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ * Serves the HTML content of the web app.
+ * This function is automatically called when the web app URL is accessed.
+ * @returns {HtmlOutput} The HTML content to be displayed.
+ */
+
+function doGet() {
+  // Create a template from the 'cafe-calorie.html' file
+  return HtmlService.createTemplateFromFile('cafe-calorie')
+      .evaluate() // Process any server-side includes or scripts in the HTML
+      .setTitle('Cafe Calorie App'); // Set the title of the web page
+      // Removed .setXFrameOptions(HtmlService.XFrameOptions.ALLOWALL) to resolve TypeError
+}
+    
